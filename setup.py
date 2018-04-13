a78590e34d1204eec1a3a81643181fb43e40482d
Maurice Marx <momomarx@gmail.com>
2018-04-13 13:21:30 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='trackviz',
+    version='0.1',
+    description='Visualize trajectories in 2D and 3D using matplotlib',
+    author='Maurice Marx',
+    author_email='momomarx@gmail.com',
+    license='MIT',
+    packages=['trackviz'],
+    install_requires=[]
+)
